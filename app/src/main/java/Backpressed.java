1e2a595f2583e36b5217e95ba9ce78452e50e6b3
bakjonie <gjonmadesty@gmail.com>
2016-11-01 01:44:31 -0700

    On back press

---
+++ app/src/main/java/Backpressed.java
@@ -0,0 +1,38 @@
+package com.madesty.madestymwesogame;
+/**
+ * Created by Carlos on 10/29/2016.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+public class Backpressed {
+    public static void main(String[] args) {
+
+        public void onBackPressed () {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("Exit Application?");
+            alertDialogBuilder
+                    .setMessage("Click yes to exit!")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    moveTaskToBack(true);
+                                    android.os.Process.killProcess(android.os.Process.myPid());
+                                    System.exit(1);
+                                }
+                            })
+
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
+    }
+}
